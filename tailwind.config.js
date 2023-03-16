2afe1dddf72bed9c3efb08ad1fa516b50208a9e5
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-03-16 19:22:31 +0700

    make first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        crimson : '#dc2626',
+        dark: '#0f172a',
+        'secondary' : '#64738b',
+      },
+      screens: {
+        "2xl" : '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
