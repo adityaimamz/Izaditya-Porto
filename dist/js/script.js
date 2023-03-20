0dc613bf6f4931f1ac6b2514efb067d064090bd1
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-03-20 08:12:03 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,17 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
+    const top = document.querySelector('#top');
     const fixedNav = header.offsetTop;
+
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        top.classList.remove('hidden');
+        top.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        top.classList.remove('flex');
+        top.classList.add('hidden');
     }
 }
 
@@ -16,3 +22,11 @@ humberger.addEventListener('click', function () {
     humberger.classList.toggle('humberger-active');
     navMenu.classList.toggle('hidden');
 });
+
+
+window.addEventListener('click', function(e) {
+    if(e.target != humberger &&  e.target != navMenu) {
+        humberger.classList.remove('humberger-active');
+        navMenu.classList.add('hidden')
+    }
+})
